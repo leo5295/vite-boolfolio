3c9fe444a78d1a5ae01afa252449f0c7425e8fa3
leo5295 <leonardo.martellosio@gmail.com>
2023-03-09 18:37:00 +0100

    done, img problem to fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup>
 import Home from './components/Home.vue'
-import axios from 'axios';
 
-axios.get('http://127.0.0.1:8000/api/users')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
 </script>
 
 <template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,31 @@
 <template lang="">
-    <div>
-        <h1>Dario non mi aiuta</h1>
-    </div>
+     <div>
+    <h2>Lista degli articoli</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h3>{{ project.title }}</h3>
+        <p>{{ project.content }}</p>
+        <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'">
+    </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            projects: [],
+            baseUrl: 'http://127.0.0.1:8000/',
+        }
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/users').then(response => {
+            this.projects = response.data.results;
+        });
+    }
 }
 </script>
 
