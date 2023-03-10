3fc3f2bc0da5bd0477a70343af9252b6b7c5bcc7
leo5295 <leonardo.martellosio@gmail.com>
2023-03-10 15:08:47 +0100

    fixed, done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,11 @@
     <h2>Lista degli articoli</h2>
     <ul>
       <li v-for="project in projects" :key="project.id">
-        <h3>{{ project.title }}</h3>
-        <p>{{ project.content }}</p>
-        <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'">
+        <div class=card>
+            <h3>{{ project.title }}</h3>
+            <p>{{ project.content }}</p>
+            <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'">
+        </div>
     </li>
     </ul>
   </div>
@@ -18,11 +20,11 @@ export default {
     data() {
         return {
             projects: [],
-            baseUrl: 'http://127.0.0.1:8000/',
+            baseUrl: 'http://127.0.0.1:8000',
         }
     },
     mounted() {
-        axios.get('http://127.0.0.1:8000/api/users').then(response => {
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
             this.projects = response.data.results;
         });
     }
