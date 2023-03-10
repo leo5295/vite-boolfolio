459c0871fe5738fd5531fa79166d8272d88df09e
leo5295 <leonardo.martellosio@gmail.com>
2023-03-10 15:33:37 +0100

    add technologies and types at project

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,16 @@
      <div>
     <h2>Lista degli articoli</h2>
     <ul>
-      <li v-for="project in projects" :key="project.id">
-        <div class=card>
-            <h3>{{ project.title }}</h3>
-            <p>{{ project.content }}</p>
-            <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'">
-        </div>
-    </li>
+      <div v-for="project in projects" :key="project.id" class="card" >
+        <h3>{{ project.title }}</h3>
+        <p>{{ project.content }}</p>
+        <p>{{ project.type.name }}</p>
+        <p>{{ project.content }}</p>
+        <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'">
+        <span v-for="technology in project.technologies">
+        {{technology.name}}
+        </span>
+    </div>
     </ul>
   </div>
 </template>
