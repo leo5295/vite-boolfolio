4b5bd9ed27a15a484351b6e59c9bc3726f6329c6
leo5295 <leonardo.martellosio@gmail.com>
2023-03-12 23:03:25 +0100

    index, route, main done

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
-<script setup>
-import Home from './components/Home.vue'
-
-</script>
-
 <template>
   <div>
-    <Home />
+    <!-- <Home /> -->
+    <router-view />
   </div>
 </template>
+
+<script setup>
+import Home from './components/Home.vue'
+</script>
 
 <style scoped lang="scss">
 @use "./styles/generals.scss" as *;
 </style>
+
